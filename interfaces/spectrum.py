acf73e82bb2923157f45ecf22f2eb977e31a9df8
Jaladh Singhal <jaladhsinghal@gmail.com>
2019-12-12 22:57:11 +0530

    Add button to download the plotted spectrum as CSV

--- interfaces/spectrum.py
+++ interfaces/spectrum.py
@@ -1,10 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-
+from dash.dependencies import Input, Output, State
+import flask
 import plotly.graph_objs as go
 
+import os
+import io
+import pandas as pd
 from starkit.gridkit import load_grid
 
 
@@ -67,7 +70,11 @@ app.layout = html.Div([
     generate_slider('T eff', 'teff_slider', 1, teff_extent),
     generate_slider('log g', 'logg_slider', 0.1, logg_extent),
     generate_slider('[M/H]', 'mh_slider', 0.1, mh_extent),
-    dcc.Graph(id='spectrum_graph')
+    dcc.Graph(id='spectrum_graph'),
+    html.A(
+        id='download_btn',
+        children=html.Button('Download This Spectrum')
+    )
 ])
 
 
@@ -96,6 +103,43 @@ def plot_spectrum(selected_teff, selected_logg, selected_mh):
                 'exponentformat': 'e'}
             )
         }
+
+
+@app.callback(
+    Output('download_btn', 'href'), #arg2 = action
+    [Input('teff_slider', 'value'),
+     Input('logg_slider', 'value'),
+     Input('mh_slider', 'value')])
+def update_href(selected_teff, selected_logg, selected_mh):
+    return '/downloadSpectrum?teff={0}&logg={1}&mh={2}'.format(
+        selected_teff, selected_logg, selected_mh)
+
+
+@app.server.route('/downloadSpectrum')
+def download_spectrum():
+    selected_teff = flask.request.args.get('teff')
+    selected_logg = flask.request.args.get('logg')
+    selected_mh = flask.request.args.get('mh')
+    print("\n\n",selected_teff, selected_logg, selected_mh, "\n\n")
+    fname = 'pheonix_teff{0}_logg{1}_mh{2}.csv'.format(
+        selected_teff, selected_logg, selected_mh)
+    
+    grid.teff = selected_teff
+    grid.logg = selected_logg
+    grid.mh = selected_mh
+    wave, flux = grid()
+
+    str_io = io.StringIO()
+    data = pd.DataFrame({'Wavelength': wave, 'Flux': flux})
+    data.to_csv(str_io, index=False)
+    mem = io.BytesIO()
+    mem.write(str_io.getvalue().encode('utf-8'))
+    mem.seek(0)
+    str_io.close()
+    return flask.send_file(mem,
+                       mimetype='text/csv',
+                       attachment_filename=fname,
+                       as_attachment=True)
 
 
 if __name__ == '__main__':
